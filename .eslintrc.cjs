7cbe85ae73fbf60b9adcb2208c2d10e4ff2c99c4
Matt DeKrey <mattdekrey@gmail.com>
2022-05-17 07:07:51 -0500

    Correct eslint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,11 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
+	root: true,
 
 	parserOptions: {
-		ecmaVersion: 2020,
-		sourceType: 'module',
 		project: './tsconfig.eslint.json',
+		tsconfigRootDir: __dirname,
 	},
+	ignorePatterns: ['node_modules/'],
 
 	env: {
 		browser: true,
@@ -13,10 +13,17 @@ module.exports = {
 
 	extends: ['plugin:@typescript-eslint/recommended', 'plugin:jest/recommended', 'plugin:prettier/recommended'],
 
-	plugins: ['@typescript-eslint', 'jest'],
+	plugins: ['prettier', 'jest'],
 
 	rules: {
-		// Specify any specific ESLint rules.
+		'@typescript-eslint/no-shadow': 0,
+		'import/prefer-default-export': 0,
+		'@typescript-eslint/explicit-module-boundary-types': 0,
+		'no-nested-ternary': 0,
+		'no-unneeded-ternary': 0,
+		'global-require': 0,
+		'@typescript-eslint/no-use-before-define': 0,
+		'comma-dangle': ['error', 'always-multiline'],
 	},
 
 	overrides: [
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
-  "useTabs": true,
-  "printWidth": 120,
-  "bracketSameLine": true,
-  "singleQuote": true
+	useTabs: true,
+	printWidth: 120,
+	bracketSameLine: true,
+	singleQuote: true,
 };
