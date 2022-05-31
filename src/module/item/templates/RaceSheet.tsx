1e3278e32edb30b2d8b3b0bf518e8fc5a3d2759f
Matt DeKrey <mattdekrey@gmail.com>
2022-05-31 13:07:14 -0500

    Convert race template

---
+++ src/module/item/templates/RaceSheet.tsx
@@ -0,0 +1,35 @@
+import { FormInput } from 'src/components/form-input';
+import { ImageEditor } from 'src/components/image-editor';
+import { Bonuses } from '../components/bonuses';
+import { SpecificItem } from '../mashup-item';
+
+export function RaceSheet({ item }: { item: SpecificItem<'race'> }) {
+	return (
+		<div className="h-full flex flex-col gap-1">
+			<div className="flex flex-row gap-1">
+				<ImageEditor document={item} field="img" title={item.name} className="w-24 h-24 border-2 border-black p-px" />
+				<div className="grid grid-cols-12 grid-rows-2 gap-x-1 items-end">
+					<FormInput className="col-span-12">
+						<FormInput.AutoTextField document={item} field="name" className="text-lg" />
+						<FormInput.Label>Race Name</FormInput.Label>
+					</FormInput>
+					<FormInput className="col-span-2">
+						<FormInput.Field>
+							<div className="flex items-baseline">
+								<FormInput.AutoNumberField
+									plain
+									document={item}
+									field="data.baseSpeed"
+									className="text-lg text-center min-w-0 flex-shrink"></FormInput.AutoNumberField>
+								<span>sq.</span>
+							</div>
+						</FormInput.Field>
+						<FormInput.Label>Base Speed</FormInput.Label>
+					</FormInput>
+				</div>
+			</div>
+			<div className="border-b border-black"></div>
+			<Bonuses document={item} field="data.grantedBonuses" className="flex-grow" />
+		</div>
+	);
+}
--- src/module/item/templates/sheet.tsx
+++ src/module/item/templates/sheet.tsx
@@ -2,6 +2,7 @@ import { SpecificItem } from '../mashup-item';
 import { MashupItemSheet } from '../mashup-item-sheet';
 import { PossibleItemData } from '../types';
 import { ClassSheet } from './ClassSheet';
+import { RaceSheet } from './RaceSheet';
 
 function isItemType<T extends PossibleItemData['type']>(item: SpecificItem, type: T): item is SpecificItem<T> {
 	return item.data.type === type;
@@ -10,5 +11,9 @@ function isItemType<T extends PossibleItemData['type']>(item: SpecificItem, type
 export function ItemSheetJsx({ sheet }: { sheet: MashupItemSheet }) {
 	const item = sheet.item as SpecificItem;
 
-	return isItemType(item, 'class') ? <ClassSheet item={item} /> : null;
+	return isItemType(item, 'class') ? (
+		<ClassSheet item={item} />
+	) : isItemType(item, 'race') ? (
+		<RaceSheet item={item} />
+	) : null;
 }
