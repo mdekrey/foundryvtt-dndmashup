bdd5e4f52146dda65fc0a70a909f173d219b20a8
Matt DeKrey <mattdekrey@gmail.com>
2022-06-11 07:24:56 -0500

    Handle invalid values on blur

--- src/components/form-input/hooks/useKeyValueWhenBlur.ts
+++ src/components/form-input/hooks/useKeyValueWhenBlur.ts
@@ -1,4 +1,5 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 type Result<T> = {
 	defaultValue: T;
@@ -7,21 +8,36 @@ type Result<T> = {
 	onFocus: () => void;
 };
 
+/*
+
+Use cases:
+
+1. We are typing anywhere in it and it is valid and updates, so we don't want the cursor to move
+2. We blur, but it was invalid, so it needs to change back to last valid value
+3. We are blurred and something else changes the value, so it needs to show the new value
+
+*/
+
 export function useKeyValueWhenBlur<T extends React.Key>(value: T): Result<T>;
 export function useKeyValueWhenBlur<T>(value: T, convert: (value: T) => React.Key): Result<T>;
 export function useKeyValueWhenBlur<T>(value: T, convert?: (value: T) => React.Key): Result<T> {
 	const converted = convert ? convert(value) : (value as never as React.Key);
 	const key = useRef(converted);
 	const isFocused = useRef(false);
+	const [rerenderKey, forceRerender] = useState('');
 
 	if (!isFocused.current && key.current !== converted) {
-		key.current = converted;
+		key.current = converted + rerenderKey;
 	}
 
 	return {
 		defaultValue: value,
 		key: key.current,
-		onBlur: () => void (isFocused.current = false),
+		onBlur: () => {
+			isFocused.current = false;
+			key.current = uuid();
+			forceRerender(key.current);
+		},
 		onFocus: () => void (isFocused.current = true),
 	};
 }
