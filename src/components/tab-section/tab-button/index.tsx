13d430e49cab51b2c27783da7f9e947ce98021e1
Matt DeKrey <mattdekrey@gmail.com>
2022-06-16 06:49:42 -0500

    Add keyboard visibilty to tab buttons

--- src/components/tab-section/tab-button/index.tsx
+++ src/components/tab-section/tab-button/index.tsx
@@ -8,9 +8,12 @@ export function TabButton({ tabName, children }: { tabName: string; children?: R
 		<button
 			type="button"
 			onClick={() => setActiveTab(tabName)}
-			className={classNames('ring-transparent hover:ring-blue-bright-600 cursor-pointer ring-text-shadow uppercase', {
-				'font-bold': activeTab === tabName,
-			})}>
+			className={classNames(
+				'ring-transparent hover:ring-blue-bright-600 focus:ring-blue-bright-600 cursor-pointer ring-text-shadow uppercase',
+				{
+					'font-bold': activeTab === tabName,
+				}
+			)}>
 			{children}
 		</button>
 	);
