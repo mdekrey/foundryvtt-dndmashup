18628129877a6d9fdfcd849d41b2a0f1029a88e6
Matt DeKrey <matt.dekrey@principlestudios.com>
2022-07-03 07:45:28 -0500

    Convert more auto fields

--- src/module/item/subtypes/feature/FeatureSheet.tsx
+++ src/module/item/subtypes/feature/FeatureSheet.tsx
@@ -1,30 +1,46 @@
-import { FormInput } from 'src/components/form-input';
+import { FormInput, SelectItem } from 'src/components/form-input';
 import { ImageEditor } from 'src/components/image-editor';
 import { Bonuses } from 'src/module/bonuses';
 import { Tabs } from 'src/components/tab-section';
 import { FeaturesList } from '../../components/FeaturesList';
-import { featureTypes, MashupItemFeature } from './config';
+import { FeatureType, featureTypes, MashupItemFeature } from './config';
+import { documentAsState } from 'src/components/form-input/hooks/useDocumentAsState';
+import { SourceDataOf } from 'src/core/foundry';
+import { Lens } from 'src/core/lens';
 
-const options = Object.entries(featureTypes).map(([key, { label: label }]) => ({ value: key, key, label }));
+const options = Object.entries(featureTypes).map(
+	([key, { label: label }]): SelectItem<FeatureType> => ({
+		value: key as FeatureType,
+		key,
+		label,
+		typeaheadLabel: label,
+	})
+);
+
+const baseLens = Lens.identity<SourceDataOf<MashupItemFeature>>();
 
 export function FeatureSheet({ item }: { item: MashupItemFeature }) {
+	const documentState = documentAsState(item);
 	return (
 		<div className="h-full flex flex-col gap-1">
 			<div className="flex flex-row gap-1">
 				<ImageEditor document={item} field="img" title={item.name} className="w-24 h-24 border-2 border-black p-px" />
-				<div className="grid grid-cols-12 grid-rows-2 gap-x-1 items-end flex-grow">
-					<FormInput className="col-span-10">
-						<FormInput.AutoTextField document={item} field="name" className="text-lg" />
+				<div className="grid grid-cols-12 grid-rows-2 gap-x-1 items-end flex-grow text-lg">
+					<FormInput className="col-span-9">
+						<FormInput.TextField {...baseLens.toField('name').apply(documentState)} />
 						<FormInput.Label>Feature Name</FormInput.Label>
 					</FormInput>
-					<FormInput className="col-span-2">
+					<FormInput className="col-span-3">
 						<FormInput.Field>
-							<FormInput.AutoSelect document={item} options={options} field="data.featureType" />
+							<FormInput.Select
+								{...baseLens.toField('data').toField('featureType').apply(documentState)}
+								options={options}
+							/>
 						</FormInput.Field>
 						<FormInput.Label>Type</FormInput.Label>
 					</FormInput>
 					<FormInput className="col-span-12">
-						<FormInput.AutoTextField document={item} field="data.summary" className="text-lg" />
+						<FormInput.TextField {...baseLens.toField('data').toField('summary').apply(documentState)} />
 						<FormInput.Label>Summary</FormInput.Label>
 					</FormInput>
 				</div>
