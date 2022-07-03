841038cc60a5f88319c2efe1a0ec8fef12328c2e
Matt DeKrey <matt.dekrey@principlestudios.com>
2022-07-02 23:19:59 -0500

    Use structured field

--- src/module/item/subtypes/race/RaceSheet.tsx
+++ src/module/item/subtypes/race/RaceSheet.tsx
@@ -10,22 +10,18 @@ export function RaceSheet({ item }: { item: MashupItemRace }) {
 		<div className="h-full flex flex-col gap-1">
 			<div className="flex flex-row gap-1">
 				<ImageEditor document={item} field="img" title={item.name} className="w-24 h-24 border-2 border-black p-px" />
-				<div className="grid grid-cols-12 grid-rows-2 gap-x-1 items-end flex-grow">
+				<div className="grid grid-cols-12 grid-rows-2 gap-x-1 items-end flex-grow text-lg">
 					<FormInput className="col-span-12">
-						<FormInput.AutoTextField document={item} field="name" className="text-lg" />
+						<FormInput.AutoTextField document={item} field="name" />
 						<FormInput.Label>Race Name</FormInput.Label>
 					</FormInput>
 					<FormInput className="col-span-2">
-						<FormInput.Field>
-							<div className="flex items-baseline">
-								<FormInput.AutoNumberField
-									plain
-									document={item}
-									field="data.baseSpeed"
-									className="text-lg text-center min-w-0 flex-shrink"></FormInput.AutoNumberField>
+						<FormInput.Structured>
+							<FormInput.Structured.Main>
+								<FormInput.AutoNumberField plain document={item} field="data.baseSpeed" />
 								<span>sq.</span>
-							</div>
-						</FormInput.Field>
+							</FormInput.Structured.Main>
+						</FormInput.Structured>
 						<FormInput.Label>Base Speed</FormInput.Label>
 					</FormInput>
 				</div>
