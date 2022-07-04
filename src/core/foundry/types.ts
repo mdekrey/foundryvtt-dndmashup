75775ba8cbeedb27b89499345bddc312f0140b5f
Matt DeKrey <matt.dekrey@principlestudios.com>
2022-07-03 22:55:30 -0500

    Add possible typing improvement

--- src/core/foundry/types.ts
+++ src/core/foundry/types.ts
@@ -4,6 +4,8 @@ import {
 	DocumentData,
 } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/abstract/module.mjs';
 import { ClientDocumentConstructor } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/client/data/abstract/client-document';
+import { PropertiesToSource } from '@league-of-foundry-developers/foundry-vtt-types/src/types/helperTypes';
+import { ItemDataBaseProperties } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/data/data.mjs/itemData';
 
 export type AnyDocument = InstanceType<
 	ClientDocumentConstructor<ConstructorOf<Document<DocumentData<any, any, any, any, any>, any, any>>>
@@ -18,3 +20,7 @@ export type SourceDataOf<T extends AnyDocument> = DataOfDocument<T> extends Docu
 >
 	? TData
 	: never;
+export type SourceDataOnlyOf<T extends AnyDocument> =
+	SourceDataOf<T> extends PropertiesToSource<ItemDataBaseProperties> & infer U
+		? U & Omit<ItemDataBaseProperties, 'data'>
+		: never;
