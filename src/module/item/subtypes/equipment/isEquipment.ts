6dae1fb36bfc81b4a7173fd83feaeaf9e9a8b0b7
Matt DeKrey <matt.dekrey@principlestudios.com>
2022-07-10 11:53:13 -0500

    Correct isEquipment

--- src/module/item/subtypes/equipment/isEquipment.ts
+++ src/module/item/subtypes/equipment/isEquipment.ts
@@ -1,17 +1,15 @@
 import { SimpleDocument } from 'src/core/interfaces/simple-document';
 import { MashupItemBase } from '../../mashup-item';
-import { MashupItemEquipment } from '.';
-import { EquipmentData, EquipmentDocument } from './dataSourceData';
+import { MashupItemEquipment } from './config';
+import { EquipmentDocument } from './dataSourceData';
 import { ItemSlot } from './item-slots';
 
 export function isEquipment(item: SimpleDocument): item is EquipmentDocument;
-export function isEquipment<T extends ItemSlot>(item: SimpleDocument, itemType: T): item is EquipmentDocument<T>;
 export function isEquipment(item: MashupItemBase): item is MashupItemEquipment;
-export function isEquipment(
-	item: SimpleDocument | MashupItemBase,
-	itemType?: ItemSlot
-): item is MashupItemEquipment | EquipmentDocument {
-	return (
-		item.type === 'equipment' && (itemType === undefined || (item.data as EquipmentData).data.itemSlot === itemType)
-	);
+export function isEquipment(item: SimpleDocument | MashupItemBase): item is MashupItemEquipment | EquipmentDocument {
+	return item.type === 'equipment';
+}
+
+export function isEquipmentSlot<T extends ItemSlot>(item: SimpleDocument, itemType: T): item is EquipmentDocument<T> {
+	return isEquipment(item) && item.data.data.itemSlot === itemType;
 }
