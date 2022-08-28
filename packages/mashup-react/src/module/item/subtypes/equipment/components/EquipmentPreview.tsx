f52d8ad3d7db65470ec85e04d0c63e4c48d45bb5
Matt DeKrey <mattdekrey@gmail.com>
2022-08-28 07:43:44 -0500

    Allows all equipment to be shared
    Resolves #6

--- packages/mashup-react/src/module/item/subtypes/equipment/components/EquipmentPreview.tsx
+++ packages/mashup-react/src/module/item/subtypes/equipment/components/EquipmentPreview.tsx
@@ -1,11 +1,25 @@
 // import { useApplicationDispatcher } from '@foundryvtt-dndmashup/foundry-compat';
 import classNames from 'classnames';
 import { cardRowFormat, FlavorText, RulesText } from '@foundryvtt-dndmashup/components';
-import { oxfordComma, pipeJsx } from '@foundryvtt-dndmashup/core';
+import { ensureSign, oxfordComma, pipeJsx } from '@foundryvtt-dndmashup/core';
 import { EquipmentDocument } from '../dataSourceData';
 import { itemSlots } from '../item-slots';
-import { bonusToText, DynamicListTarget, dynamicListTargetNames } from '@foundryvtt-dndmashup/mashup-rules';
+import {
+	bonusToText,
+	DynamicListTarget,
+	dynamicListTargetNames,
+	PoolBonusTarget,
+} from '@foundryvtt-dndmashup/mashup-rules';
 import { groupBy } from 'lodash/fp';
+import { isEquipment } from '../isEquipment';
+import { isPower, PowerPreview } from '../../power';
+
+const poolBonusTargetText: Record<PoolBonusTarget, string> = {
+	max: 'maximum uses',
+	longRest: 'uses gained per long rest',
+	shortRest: 'uses gained per short rest',
+	perRest: 'allowed uses between short and long rests',
+};
 
 export function EquipmentPreview({ item, simple }: { item: EquipmentDocument; simple?: boolean }) {
 	const { name, data: itemData } = item.data;
@@ -14,6 +28,14 @@ export function EquipmentPreview({ item, simple }: { item: EquipmentDocument; si
 	const flavorText = itemData.description.text ? (
 		<FlavorText dangerouslySetInnerHTML={{ __html: itemData.description.text }} />
 	) : null;
+
+	const equipmentContents = item.data.data.container
+		? item.items.contents
+				.filter(isEquipment)
+				.map((eq) => eq.name)
+				.filter((n): n is string => n !== null)
+		: [];
+	const powerContents = item.items.contents.filter(isPower);
 
 	return (
 		<section className={classNames('bg-white', { 'cursor-pointer': simple })} onClick={simple ? onDetails : undefined}>
@@ -44,14 +66,34 @@ export function EquipmentPreview({ item, simple }: { item: EquipmentDocument; si
 								{oxfordComma(entries.map((e) => e.entry))}
 							</RulesText>
 						))}
+						{Object.entries(groupBy((e) => e.name, item.data.data.grantedPoolBonuses ?? [])).map(
+							([pool, poolBonuses], index) => (
+								<RulesText label={pool} key={pool}>
+									{poolBonuses
+										.map((poolBonus) => `${ensureSign(poolBonus.amount)} to ${poolBonusTargetText[poolBonus.target]}.`)
+										.join(' ')}
+								</RulesText>
+							)
+						)}
+						{item.data.data.container ? (
+							<RulesText label="Contents">
+								{equipmentContents.length ? oxfordComma(equipmentContents) : 'None'}
+							</RulesText>
+						) : null}
 					</>
 				),
 				cardRowFormat
 			)}
+			<div className="ml-4">
+				{powerContents.map((power, index) => (
+					<PowerPreview item={power} key={item.id ?? index} />
+				))}
+			</div>
 		</section>
 	);
 
 	function onDetails() {
+		// TODO
 		// applications.launchApplication('equipmentDetails', { item });
 	}
 }
